6efe60d0799b76c0e6c1429e621ae63589633399
TamasSomi <>
2023-12-28 14:45:15 +0000

    fix typo in setting and env cloudinary link, create profiles app

--- productive_app/settings.py
+++ productive_app/settings.py
@@ -47,7 +47,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'cloudinary_storge',
+    'cloudinary_storage',
     'django.contrib.staticfiles',
     'cloudinary',
 ]
