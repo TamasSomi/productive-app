774712e54089e2c2b894e8b1cb51c0e3a6cf99e3
TamasSomi <>
2024-01-07 19:57:25 +0000

    add image_filter with choices and size limitation

--- tasks/models.py
+++ tasks/models.py
@@ -4,9 +4,26 @@ from django.contrib.auth.models import User
 
 class Task(models.Model):
     """
-    Post model, related to 'owner', i.e. a User instance.
-    Default image set so that we can always reference image.url.
+
     """
+
+    image_filter_choices = [
+        ('_1977', '1977'),
+        ('brannan', 'Brannan'),
+        ('earlybird', 'Earlybird'),
+        ('hudson', 'Hudson'),
+        ('inkwell', 'Inkwell'),
+        ('lofi', 'Lo-Fi'),
+        ('kelvin', 'Kelvin'),
+        ('normal', 'Normal'),
+        ('nashville', 'Nashville'),
+        ('rise', 'Rise'),
+        ('toaster', 'Toaster'),
+        ('valencia', 'Valencia'),
+        ('walden', 'Walden'),
+        ('xpro2', 'X-pro II')
+    ]
+
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
@@ -15,6 +32,9 @@ class Task(models.Model):
     deadline = models.DateTimeField(null=True, blank=True)
     image = models.ImageField(
         upload_to='images/', default='../default_post_wadwig', blank=True
+    )
+    image_filter = models.CharField(
+        max_length=32, choices=image_filter_choices, default='normal'
     )
 
     class Meta:
