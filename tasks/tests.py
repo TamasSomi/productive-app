1e223a9ab27f6f64d1d2c0558df0b5d13bb967f6
TamasSomi <>
2024-01-08 14:49:20 +0000

    add tasklist view tests

--- tasks/tests.py
+++ tasks/tests.py
@@ -1,3 +1,28 @@
-from django.test import TestCase
+from django.contrib.auth.models import User
+from .models import Task
+from rest_framework import status
+from rest_framework.test import APITestCase
 
-# Create your tests here.
+
+class TaskListViewTests(APITestCase):
+    def setUp(self):
+        User.objects.create_user(username='tomi', password='pass')
+
+    def test_can_list_tasks(self):
+        tomi = User.objects.get(username='tomi')
+        Task.objects.create(owner=tomi, title='a title')
+        response = self.client.get('/tasks/')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        print(response.data)
+        print(len(response.data))
+
+    def test_logged_in_user_can_create_task(self):
+        self.client.login(username='tomi', password='pass')
+        response = self.client.post('/tasks/', {'title': 'a title'})
+        count = Task.objects.count()
+        self.assertEqual(count, 1)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+    def test_user_not_logged_in_cant_create_task(self):
+        response = self.client.post('/tasks/', {'title': 'a title'})
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
