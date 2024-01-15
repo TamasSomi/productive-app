4f5f115dedeb2299d207ea7d8dac80d6a2116150
TamasSomi <>
2024-01-15 20:57:31 +0000

    fix api_view method error

--- productive_app/views.py
+++ productive_app/views.py
@@ -14,7 +14,7 @@ def root_route(request):
 
 
 # dj-rest-auth logout view fix
-@api_view(['TASK'])
+@api_view(['POST'])
 def logout_route(request):
     response = Response()
     response.set_cookie(
